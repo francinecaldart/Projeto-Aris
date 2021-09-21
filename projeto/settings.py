03713ae89545b5da260e28bca9369671534f7db4
francinecaldart <90914273+francinecaldart@users.noreply.github.com>
2021-09-21 14:31:03 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -133,7 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/image/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR,'static')
