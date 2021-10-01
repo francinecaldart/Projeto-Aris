faebf4c5bf5e461521fd301280b8c72c1ae4e960
francinecaldart <90914273+francinecaldart@users.noreply.github.com>
2021-10-01 14:02:56 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -86,12 +86,9 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 
